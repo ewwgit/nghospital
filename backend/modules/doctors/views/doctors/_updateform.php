7957dc80b24f8b91a2e074212ef7ec11b34db3c4
santosh <santosh@EWW18.eww.local>
2017-06-14 13:21:35 +0530

    Updated for form fields alignments

--- backend/modules/doctors/views/doctors/_updateform.php
+++ backend/modules/doctors/views/doctors/_updateform.php
@@ -13,42 +13,45 @@ use yii\helpers\Url;
 <div class="doctors-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-    
+    </div>
+    <div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-    
-   
-
+    </div>
+   	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'qualification')->textarea(['rows' => 6]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
+    <?= $form->field($model, 'qualification')->textInput(['maxlength' => true]) ?>
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'state')->textInput() ?>
-
-    
-
+	</div>
+    <div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'country')->textInput() ?>
-
-   
-
-    <?= $form->field($model, 'address')->textarea(['rows' => 6]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'pinCode')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'doctorMobile')->textInput(['maxlength' => true]) 
 
     // $form->field($model, 'doctorImage')->textInput() ?>
-     <?php if($model->doctorImage != ''){?>
-    
+    </div>
+    <div class="form-group col-lg-6 col-sm-12">
+    <?php if($model->doctorImage != ''){?>
+    </div>
+    <div class="form-group col-lg-6 col-sm-12">
     <?php $imgeurl = str_replace("frontend","backend",Yii::getAlias('@web/')).$model->doctorImage;?>
-   
+
 						 		
 						 		<?php  }
 						 		// print_r($imgeurl);exit;?>
- 
+
 						<h5>Upload Your Profile Image</h5>
 						<img class='image' 
 							src="<?php
@@ -62,19 +65,20 @@ use yii\helpers\Url;
 								      }
 								?>"
 							width="100" height="100"> </img> 
-            <?=$form->field ( $model, 'doctorImage' )->widget ( FileInput::classname (), [ 'options' => [ 'accept' => 'image/*' ],'pluginOptions' =>[[ 'browseLabel' => 'doctorImage', 'allowedFileExtensions'=>['jpg','png','jpeg'] ]] ] )?>
-            
-            
-        
-               
-
-    <?= $form->field($model, 'summery')->textarea(['rows' => 6]) ?>
-
+	</div>
+	
+    <div class="form-group col-lg-6 col-sm-12">
+    <?= $form->field($model, 'address')->textarea(['rows' => 4]) ?>
+	</div>
+    <div class="form-group col-lg-6 col-sm-12">
+    <?= $form->field($model, 'summery')->textarea(['rows' => 4]) ?>
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'APMC')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'TSMC')->textInput(['maxlength' => true]) ?>
-
-   
+	</div>   
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- backend/modules/nursinghomes/views/nursinghomes/_updateform.php
+++ backend/modules/nursinghomes/views/nursinghomes/_updateform.php
@@ -11,28 +11,36 @@ use yii\widgets\ActiveForm;
 <div class="nursinghomes-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'contactPerson')->textInput(['maxlength' => true]) ?>
-    
+    </div>
+    <div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-    
+    </div>
+    <div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-   
+   	</div>
+   	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'state')->textInput() ?>
-   
+   	</div>
+   	<div class="form-group col-lg-6 col-sm-12">
     <?= $form->field($model, 'country')->textInput() ?>
-
-   
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">   
     <?= $form->field($model, 'pinCode')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'address')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
-   
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
+    <?= $form->field($model, 'address')->textarea(['rows' => 4]) ?>
+	</div>
+	<div class="form-group col-lg-6 col-sm-12">
+    <?= $form->field($model, 'description')->textarea(['rows' => 4]) ?>
+   	</div>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
