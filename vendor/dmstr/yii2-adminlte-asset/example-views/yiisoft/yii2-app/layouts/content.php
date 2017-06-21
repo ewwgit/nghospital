367f13937226a608957d4ccf4408a3dcbddcd871
divya <divya@EWW28.anagha.local>
2017-06-21 12:59:13 +0530

    updated code commited for content copyright@

--- vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/content.php
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/content.php
@@ -39,7 +39,7 @@ use dmstr\widgets\Alert;
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.0
     </div>
-    <strong>Copyright &copy; 2016-2017 <a href="../../">Nightingale Hospitals</a>.</strong> All rights
+    <strong>Copyright &copy; <?= date("Y")?> <a href="../../">Nightingale Hospitals</a>.</strong> All rights
     reserved.
 </footer>
 
