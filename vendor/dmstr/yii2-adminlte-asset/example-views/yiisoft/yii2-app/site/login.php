08a1041cacef92fa0bd74d3ec7760b07af00c56a
santosh <santosh@EWW18.eww.local>
2017-06-28 12:18:17 +0530

    Updated for forgot password url link

--- vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/site/login.php
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/site/login.php
@@ -53,17 +53,11 @@ $fieldOptions2 = [
 
         <?php ActiveForm::end(); ?>
 
-        <div class="social-auth-links text-center">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
-                using Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign
-                in using Google+</a>
-        </div>
+        
         <!-- /.social-auth-links -->
 
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="../backend/web/index.php?r=site%2Frequest-password-reset">I forgot my password</a><br>
+        
 
     </div>
     <!-- /.login-box-body -->
