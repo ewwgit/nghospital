6f879d6adcc5d697db73af8b368ebc33de04e511
divya <divya@EWW28.anagha.local>
2017-06-28 12:47:34 +0530

    updated code commited

--- backend/modules/user/views/modulemaster/update.php
+++ backend/modules/user/views/modulemaster/update.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\ModulesMaster */
 
-$this->title = 'Update Modules Master: ' . $model->moduleName;
-$this->params['breadcrumbs'][] = ['label' => 'Modules Masters', 'url' => ['index']];
+$this->title = 'Update Modules : ' . $model->moduleName;
+$this->params['breadcrumbs'][] = ['label' => 'Modules ', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->moduleName, 'url' => ['view', 'id' => $model->moduleId]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
