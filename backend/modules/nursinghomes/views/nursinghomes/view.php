eee8377eae626120d5e4ec373a2b4dfd7eaa5481
divya <divya@EWW28.anagha.local>
2017-07-22 10:47:10 +0530

    updated code commited for  doctor and nursing print view

--- backend/modules/nursinghomes/views/nursinghomes/view.php
+++ backend/modules/nursinghomes/views/nursinghomes/view.php
@@ -42,8 +42,8 @@ th {
 }
 </style>
     <div class="row">      
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" style="margin-left:0px;">			   
-			<div class="panel panel-info">	
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >			   
+			<div class="panel ">	
 			
 			<div class="col-md-8">	
 						<div class="box4" style="float: left;">
