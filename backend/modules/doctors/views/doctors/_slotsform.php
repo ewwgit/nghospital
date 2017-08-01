33af26a84cc36f1cced6759f3c7ceb2823ef8ab7
chary <httpchary@gmail.com>
2017-08-01 17:11:56 +0530

    updated code commited for slots form

--- backend/modules/doctors/views/doctors/_slotsform.php
+++ backend/modules/doctors/views/doctors/_slotsform.php
@@ -198,7 +198,7 @@ use unclead\multipleinput\MultipleInput;
 	 
 	
 	<div class="form-group col-lg-6 col-sm-12 " >
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Update' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end(); ?>
 
