3d0d85517bb82689e6baf92d18908338b25486db
divya <divya@EWW28.anagha.local>
2017-09-01 13:15:01 +0530

    updated code commited for roles view  update and delete options added

--- backend/modules/role/views/roles/view.php
+++ backend/modules/role/views/roles/view.php
@@ -17,6 +17,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="role-view">
 <div class="box box-primary">
 <div class="box-body">
+<p>
+        <?= Html::a('Update', ['update', 'id' => $model->RoleId], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->RoleId], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
