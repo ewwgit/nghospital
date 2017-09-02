e1ed79b646499e07037de836052bc945e0835110
chary <httpchary@gmail.com>
2017-09-02 11:35:16 +0530

    updated code commited for module master view page

--- backend/modules/user/views/modulemaster/view.php
+++ backend/modules/user/views/modulemaster/view.php
@@ -19,6 +19,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="modules-master-view">
 <div class="box box-primary">
 <div class="box-body">
+	 <p>
+        <?= Html::a('Update', ['update', 'id' => $model->moduleId], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->moduleId], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+	
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
