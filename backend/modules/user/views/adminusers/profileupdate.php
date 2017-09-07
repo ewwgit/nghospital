526611746a6fd74516bb0a29f9f35907128f3c3c
chary <httpchary@gmail.com>
2017-09-07 12:48:04 +0530

    updated code for admin page

--- backend/modules/user/views/adminusers/profileupdate.php
+++ backend/modules/user/views/adminusers/profileupdate.php
@@ -1,100 +1,107 @@
 <?php
-
 use yii\helpers\Html;
-use yii\widgets\DetailView;
-//use common\models\User;
-
+use yii\widgets\ActiveForm;
+use kartik\file\FileInput;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $model app\models\AdminMaster */
-$str = $model->username;
-$rest = substr($str, 0, 150);
-
-$this->title = $rest;
-//$this->title = $model->username;
-$this->params['breadcrumbs'][] = ['label' => 'Admin Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+/* @var $form yii\widgets\ActiveForm */
 ?>
-<div class="admin-master-view">  
-<div class="box box-primary">
-<div class="box-body">
-     <?php if($model->status == 10){
-    $status = 'Active';
-    }
-    else {
-    	$status = 'In-Active';
-    }?>
-    <?php //$sss = User::find()->one();
-    //print_r($model);exit;?>
-     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-    
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-          //  'id',
-            'username',
-           // 'email:email',
-        		[
-        		'attribute'=>'email',
-        		'value' => $model->email,
-        		//'format' => 'raw',
-        		],
-            'firstName',
-            'lastName',
-        	'phoneNumber',
-        	'address',
-        	'idproofs',
-        	
-           // 'role.RoleName',
-        		['attribute'=>'RoleName',
-        		//'label' => 'Role Name',
-        		'value' => $data,
-        		],
-        		
-        		[
-        		'attribute'=>'status',
-        		'value'=> $status,
-        		],
- 
-           // 'createdBy',
-           // 'updatedBy',
-        ],
-    		]) ?>
-    		<?php if($model->profileImage != ''){?>
-    							<?php $imgeurl = str_replace("frontend","backend",Yii::getAlias('@web/')).$model->profileImage;?>
-    							<?php  } ?>
-              <?= DetailView::widget([
-							'model' => $model,
-							'attributes' => [
-									[
-											'attribute'=>'profileImage',
-											'format' => 'html',
-											'value'=>Html::img($model->profileImage ? $imgeurl : 'images/user-iconnew.png',['width' => '150px','height' => '150px']),
-        		          ],
-                       ],
-                 ]) ?>
-</div>
-</div>
+
+<div class="admin-master-form">
+	<div class="box box-primary">
+		<div class="box-body"> 
+   <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]);  ?>
+
+  <div class="form-group col-lg-6 col-sm-12">   <?= $form->field($model, 'username')->textInput(['maxlength' => true, 'readOnly'=>($model->scenario == 'update')? "readonly" : false]) ?></div>
+
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'readOnly'=>($model->scenario == 'update')? "readonly" : false]) ?></div>
+
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'firstName')->textInput(['maxlength' => 255]) ?></div>
+
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'password')->passwordInput() ?></div>
+
+
+
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'lastName')->textInput(['maxlength' => 255]) ?></div>
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'phoneNumber')->textInput(['maxlength' => 10]) ?></div>
+
+			<div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'role')->dropDownList($model->roles, ['prompt' => 'Select Role']) ?></div>
+	        <div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'idproofs')->textArea(['rows'=>3,'maxlength'=>50]) ?></div>
+            
+            <div class="form-group col-lg-6 col-sm-12"> 
+   
+<?= $form->field($model, 'status')->dropDownList([ '10' => 'Active', '0' => 'In-active', ], ['prompt' => 'Select Status'])?>
+
 </div>
 
-<style>
-.detail-view td {
-   max-width: 50px;
+<div class="form-group col-lg-6 col-sm-12">
+    <?php if($model->profileImage != ''){?>
     
-    word-wrap: break-word;
+    <?php $imgeurl = str_replace("frontend","backend",Yii::getAlias('@web/')).$model->profileImage;?>
+
+						 		
+						 		<?php  }
+						 		// print_r($imgeurl);exit;?>
+						<img class='image' 
+							src="<?php
+							if($model->profileImage)
+							{
+								
+								echo isset( $model->profileImage)? Url::base().'/'.$model->profileImage : '' ;
+							
+							}else {
+									 echo Url::base()."/images/user-iconnew.png" ;
+								     }
+								?>"
+							width="100" height="100"> </img> 
+							 <?=$form->field ( $model, 'file' )->widget ( FileInput::classname (),
+   		[ 'options' => [ 'accept' => 'image/*' ],'pluginOptions' =>[[ 'browseLabel' => 'Profile Image', 'allowedFileExtensions'=>['jpg','png','jpeg'] ]] ] )?>  
+            <?= $form->field($model, 'profileImage')->hiddenInput()->label(false); ?>
+            
+        
+	</div>     
+			
+			
+    <div class="form-group col-lg-6 col-sm-12">  <?= $form->field($model, 'address')->textArea(['rows'=>3,'maxlength' => 255]) ?></div>    
+        	
+
+<div class="form-group  col-lg-7  col-sm-12" style="margin-top: 100px;"> 
+        <?= Html::submitButton( 'Submit', ['class' =>  'btn btn-primary'])?>
+    </div>
+			
+    
+
+</div>
+		</div>
+			
+		<?php ActiveForm::end(); ?>
+	</div>
+
+	<style>
+	
+.help-block {
+    height: 1px;
 }
 
-.detail-view th {
-   max-width: 10px;
-   
-    word-wrap: break-word;
+.form-control {
+	width: 100%;
+}
+
+.input-group {
+	width: 100%;
+}
+
+.form-group {
+	height: 120px;
+	margin-bottom: 0;
+}
+
+.profileimage {
+	margin-left: 382px;
+	margin-top: 1px;
+}
+textarea {
+    resize: none;
 }
 </style>
